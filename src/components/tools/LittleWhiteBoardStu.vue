ce956cd5f1546ebf7140ba1fb63ce0c494ae5d18
lilong <l5490624@126.com>
2020-05-28 17:20:11 +0800

    分发小白板增加pad的操作

--- src/components/tools/LittleWhiteBoardStu.vue
+++ src/components/tools/LittleWhiteBoardStu.vue
@@ -171,6 +171,9 @@
         @mousedown="canvasMouseDown"
         @mousemove="canvasMouseMove"
         @mouseup="canvasMouseUp"
+        @touchstart="touchStart"
+        @touchmove="touchMove"
+        @touchend="touchEnd"
       )
       .canvas-cover(v-if="(role === 0 && lwbStatus === 'sended') || (role !==0 && lwbStatus !== 'sended')")
     .lwb-footer
@@ -387,6 +390,103 @@ export default {
       }
     },
     canvasMouseUp() {
+      this.isDraw = false
+      switch (this.tool) {
+        case 'pencil':
+          // 如果当前路径有值，才加到allPath里面
+          if (this.curPath.length > 1) {
+            if (this.lwbStatus === 'recycled') {
+              this.allCanvasPath[this.showCanvasId].push({
+                type: 'pencil',
+                style: JSON.parse(JSON.stringify(this.style)),
+                path: this.curPath
+              })
+              this.curPath = []
+            } else {
+              this.allPath.push({
+                type: 'pencil',
+                style: JSON.parse(JSON.stringify(this.style)),
+                path: this.curPath
+              })
+              this.curPath = []
+            }
+          }
+          break;
+        case 'eraser':
+          if (this.eraserPath.length > 1) {
+            if (this.lwbStatus === 'recycled') {
+              this.allCanvasPath[this.showCanvasId].push({
+                type: 'eraser',
+                style: JSON.parse(JSON.stringify(this.style)),
+                path: this.eraserPath
+              })
+              this.eraserPath = []
+            } else {
+              this.allPath.push({
+                type: 'eraser',
+                style: JSON.parse(JSON.stringify(this.style)),
+                path: this.eraserPath
+              })
+              this.eraserPath = []
+            }
+          }
+          break
+        default:
+          break
+      }
+      // 如果allPath里面的path没有值，直接删除，避免出现空的情况
+      if (this.allPath[this.allPath.length - 1] && this.allPath[this.allPath.length - 1]['path'] && !this.allPath[this.allPath.length - 1]['path'].length) {
+        this.allPath.splice(this.allPath.length - 1)
+      }
+      // 每次绘制需要清空撤销的数组
+      this.undoArray = []
+      this.sendCanvas()
+    },
+    touchStart (e) {
+      this.isDraw = true
+      const x = e.touches[0].clientX - this.$refs.canvasRef.getBoundingClientRect().left
+      const y = e.touches[0].clientY - this.$refs.canvasRef.getBoundingClientRect().top
+      this.oldPoint.x = x
+      this.oldPoint.y = y
+    },
+    touchMove (e) {
+      this.ctx.lineCap = 'round'
+      if (this.isDraw) {
+        const x = e.touches[0].clientX - this.$refs.canvasRef.getBoundingClientRect().left
+        const y = e.touches[0].clientY - this.$refs.canvasRef.getBoundingClientRect().top
+        if (x === this.oldPoint.x && y === this.oldPoint.y) return
+        /**
+         * @todo 鼠标移出canvas区域取消绘制
+         */
+        this.style.globalCompositeOperation = 'source-over'
+        switch (this.tool) {
+          case 'pencil':
+            // 画线不需要repaint，所以在这里设置样式
+            this.ctx.strokeStyle = this.style.strokeStyle
+            this.ctx.lineWidth = this.style.lineWidth
+            this.ctx.globalCompositeOperation = this.style.globalCompositeOperation
+            this.drawPencil(this.oldPoint.x, this.oldPoint.y, x, y)
+            // push进去时，需要先除比例，避免位移偏
+            this.curPath.push([x / this.ratio, y / this.ratio])
+            this.oldPoint.x = x
+            this.oldPoint.y = y
+            break
+          case 'eraser':
+            this.style.globalCompositeOperation = 'destination-out'
+            this.ctx.globalCompositeOperation = this.style.globalCompositeOperation
+            this.ctx.strokeStyle = this.style.strokeStyle
+            this.ctx.lineWidth = this.style.lineWidth
+            this.drawEraser(this.oldPoint.x, this.oldPoint.y, x, y)
+            this.eraserPath.push([x / this.ratio, y / this.ratio])
+            this.oldPoint.x = x
+            this.oldPoint.y = y
+            break
+          default:
+            break
+        }
+      }
+    },
+    touchEnd() {
       this.isDraw = false
       switch (this.tool) {
         case 'pencil':
